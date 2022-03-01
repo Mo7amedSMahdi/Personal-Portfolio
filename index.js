bf684d6a06239443e2d1b3764191ff1f6c65fee0
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-03-01 22:47:50 +0300

    Add prodjects modal

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const mobileNav = document.getElementById('mobile_nav');
 const mobileMenuOpen = document.getElementById('mobile_menu_open');
 const mobileNavClose = document.getElementById('mobile_menu_close');
 const li = mobileNav.getElementsByTagName('a');
+const cardsContainer = document.getElementById('works');
+const buttons = cardsContainer.getElementsByTagName('button');
+const modalContainer = document.getElementById('modal');
+const modalCloseButton = document.getElementById('modalClose');
 let i;
-
+let j;
 function ShowMobileNav() {
   mobileNav.classList.remove('hidden');
   mobileMenuOpen.classList.add('hidden');
@@ -14,8 +18,20 @@ function HideMobileNav() {
   mobileMenuOpen.classList.remove('hidden');
 }
 
+function ShowModal() {
+  modalContainer.classList.remove('hidden');
+}
+
+function HideModal() {
+  modalContainer.classList.add('hidden');
+}
+
 mobileMenuOpen.addEventListener('click', ShowMobileNav);
 mobileNavClose.addEventListener('click', HideMobileNav);
+modalCloseButton.addEventListener('click', HideModal);
 for (i = 0; i < li.length; i += 1) {
   li[i].addEventListener('click', HideMobileNav);
+}
+for (j = 0; j < buttons.length; j += 1) {
+  buttons[j].addEventListener('click', ShowModal);
 }
